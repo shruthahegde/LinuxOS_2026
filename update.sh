42061d9c78bca3f710a9b56122841426accc4ef5
shruthahegde <shruthahegde10@gmail.com>
2026-08-10 15:06:36 -0400

    Add system update script

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Updating package information..."
+
 sudo apt update
 
-echo "Upgrading installed packages..."
+
 sudo apt upgrade -y
 
-echo "System update completed successfully!"
+
